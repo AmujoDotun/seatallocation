017798f82fa9c09823fb1fb0bd538b319d6f5d9f
AmujoDotun <samuelamujo@gmail.com>
2020-04-15 09:32:05 +0100

    created email template for the mail sents

---
+++ resources/views/email.blade.php
@@ -0,0 +1,17 @@
+You received a message from : {{ $name }}
+
+<p>
+Name: {{ $name }}
+</p>
+
+<p>
+Email: {{ $email }}
+</p>
+
+<p>
+Subject: {{ $subject }}
+</p>
+
+<p>
+Message: {{ $user_message }}
+</p>
